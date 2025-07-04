435bde5c75f94e8dfb72c8c1d4320236c332a332
Calvin Lua <86276701+calvinlua@users.noreply.github.com>
2025-07-05 03:15:53 +0800

    Initialize project with spring initializr
    Initialize project with dependencies:  Spring Web ( MVC Rest Controller ), Lombok ( for reducing constructor and other boilerplate code) , Spring Data JPA ( with Hibernate ORM - to diff database )  ,  H2 database

---
+++ src/main/java/com/calvin/bookstore/bookstore_javaspring/BookstoreJavaspringApplication.java
@@ -0,0 +1,13 @@
+package com.calvin.bookstore.bookstore_javaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreJavaspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreJavaspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calvin/bookstore/bookstore_javaspring/BookstoreJavaspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calvin.bookstore.bookstore_javaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreJavaspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
