5374aa22e461af9032cb18e723d338a4fb4359b1
Calvin Lua <wenzhenglua@gmail.com>
2025-07-07 11:04:36 +0800

    Fix missing annotation to Book Repository by adding @Repository
    Fix Repository with @Repository Annotation

--- src/main/java/com/calvin/bookstore/bookstore_javaspring/repository/BookRepository.java
+++ src/main/java/com/calvin/bookstore/bookstore_javaspring/repository/BookRepository.java
@@ -2,7 +2,9 @@ package com.calvin.bookstore.bookstore_javaspring.repository;
 
 import com.calvin.bookstore.bookstore_javaspring.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
     // This interface will automatically provide Database CRUD operations like CRUDRepository for the Book entity
     // Spring Data JPA automatically provides methods like save(), findById(), findAll(), deleteById()
