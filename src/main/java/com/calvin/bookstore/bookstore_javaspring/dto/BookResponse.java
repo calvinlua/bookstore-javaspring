fa35b532e2658e33e8e1e5ee022255c6a206c6de
Calvin Lua <wenzhenglua@gmail.com>
2025-07-07 18:03:45 +0800

    Update BookResponse.java
    Create DTO book response class

--- src/main/java/com/calvin/bookstore/bookstore_javaspring/dto/BookResponse.java
+++ src/main/java/com/calvin/bookstore/bookstore_javaspring/dto/BookResponse.java
@@ -1,5 +1,13 @@
 package com.calvin.bookstore.bookstore_javaspring.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class BookResponse {
     private Long id;
     private String title;
